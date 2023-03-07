6ed66d5a846794b8f05c1bc13fbd16659a332ad0
haon clement <clement.haon.13@gmail.com>
2023-03-07 22:07:57 +0100

    add requirement.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,12 +1,15 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
+token = os.environ.get('TOKEN')
 
-app = ApplicationBuilder().token("6163649593:AAG9XKllEeLiVjfvBRSbuX5oZaCeATpo0fs").build()
+app = ApplicationBuilder().token(token).build()
 
 app.add_handler(CommandHandler("hello", hello))
 
